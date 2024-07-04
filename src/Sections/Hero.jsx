bc98f6b191b9f0dcb9e2f57f6b55a0ff033d9116
MazinoCodes <jessaki180@gmail.com>
2024-07-04 23:20:06 +0100

    Hero page

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,20 +1,37 @@
 import search from '../icons/Search Icon.svg'
 import shopping from '../icons/Shopping Icon.svg'
+import drop from '../icons/DropDownHero.svg'
+import heroimg from '../images/HeroImage.svg'
+import arrow from '../icons/Arrow.svg'
 const Hero = () => {
   return (
     <div className="flex flex-col w-[100vw] px-10 pt-3  h-[100vh]">
       <div className="flex flex-row justify-between items-center">
         <h1 className="font-semibold text-xl">Crafted Charm</h1>
-        <div className="flex flex-row gap-4 text-[#747373]">
-            <a href="" className="text-[#343A40]">Home</a>
+        <div className="flex flex-row gap-4 text-[#747373] ">
+            <a href="" className="text-[#343A40] font-semibold">Home</a>
             
-            <a href="">Product</a>
+            <a href="" className="flex flex-row gap-1 items-center">Product <img src={drop} alt="" /></a>
             <a href="">Contact Us</a>
         </div>
         <div className="flex flex-row items-center gap-4">
           <img src={search} alt="" />
           <img src={shopping} alt="" />
         </div>
+      </div>
+      <div className="flex flex-row justify-around items-center ">
+        <div className="flex flex-col items-start gap-8">
+          <div className="">
+          <p className="bg-[#343A40] text-[#F5F5F5] flex flex-row items-center gap-4 w-fit px-4 rounded-[20px] ">Craft your perfect home with our furniture <img src={arrow} alt="" /></p>
+          </div>
+         <div className="flex flex-col gap-4">
+            <h1 className="text-5xl font-semibold">Elevate your Space with Crafted Charm Furniture Designed for Timeless Elegance</h1>
+            <p className="text-[#747373] text-xl">Explore our unique selection of handcrafted furniture made to add elegance and coziness to any space</p>
+         </div>
+          
+          <button className="bg-[#343A40] text-[#F5F5F5] px-4 py-3 rounded-md">Shop Now</button>
+        </div>
+        <img src={heroimg} alt="" />
       </div>
     </div>
   )
