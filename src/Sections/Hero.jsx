128b6a970eca822c358f814b8d5b08c5d5f5bca0
MazinoCodes <jessaki180@gmail.com>
2024-07-04 23:45:03 +0100

    Hero section, hero image updated

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -3,6 +3,8 @@ import shopping from '../icons/Shopping Icon.svg'
 import drop from '../icons/DropDownHero.svg'
 import heroimg from '../images/HeroImage.svg'
 import arrow from '../icons/Arrow.svg'
+import leftArrow from '../icons/LeftArrow.svg'
+import rightArrow from '../icons/RightArrow.svg'
 const Hero = () => {
   return (
     <div className="flex flex-col w-[100vw] px-10 pt-3  h-[100vh]">
@@ -19,8 +21,8 @@ const Hero = () => {
           <img src={shopping} alt="" />
         </div>
       </div>
-      <div className="flex flex-row justify-around items-center ">
-        <div className="flex flex-col items-start gap-8">
+      <div className="flex flex-row justify-around items-center w-[100vw] pr-7">
+        <div className="flex flex-col items-start gap-8 flex-1">
           <div className="">
           <p className="bg-[#343A40] text-[#F5F5F5] flex flex-row items-center gap-4 w-fit px-4 rounded-[20px] ">Craft your perfect home with our furniture <img src={arrow} alt="" /></p>
           </div>
@@ -31,7 +33,12 @@ const Hero = () => {
           
           <button className="bg-[#343A40] text-[#F5F5F5] px-4 py-3 rounded-md">Shop Now</button>
         </div>
-        <img src={heroimg} alt="" />
+        <div className="flex flex-row items-center flex-1 justify-center">
+          <button className="bg-[#343A4099] px-4 py-3 rounded-[48px]"><img src={leftArrow} alt="" /></button>
+          <img src={heroimg} alt="" />
+          <button className="bg-[#343A40] px-4 py-3 rounded-[48px] "> <img src={rightArrow} alt="" /></button>
+        </div>
+        
       </div>
     </div>
   )
