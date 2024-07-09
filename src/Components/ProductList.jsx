0c8f8cd78133056d647fa795d250c6735423d94e
MazinoCodes <jessaki180@gmail.com>
2024-07-09 09:09:49 +0100

    Search Button

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -8,14 +8,16 @@ import heartFilled from '../icons/heart-filled.svg';
 import addCart from '../icons/CartShoppingBag.svg';
 import leftArrow from '../icons/LeftArrow.svg';
 import rightArrow from '../icons/RightArrow.svg';
-import search from '../icons/Search Icon.svg'
+import search from '../icons/Search Icon.svg';
+
 const ProductList = ({ products, addToCart }) => {
   const [notificationItem, setNotificationItem] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [favorites, setFavorites] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState('All Products');
   const [filteredProducts, setFilteredProducts] = useState(products);
-  const [sortBy, setSortBy] = useState('Filter'); 
+  const [sortBy, setSortBy] = useState('Filter');
+  const [searchQuery, setSearchQuery] = useState('');
   const itemsPerPage = 12;
 
   useEffect(() => {
@@ -24,14 +26,19 @@ const ProductList = ({ products, addToCart }) => {
   }, []);
 
   useEffect(() => {
-    if (selectedFilter === 'All Products') {
-      setFilteredProducts(products);
-    } else {
-      const filtered = products.filter(product => product.category === selectedFilter);
-      setFilteredProducts(filtered);
+    let updatedProducts = products;
+
+    if (selectedFilter !== 'All Products') {
+      updatedProducts = updatedProducts.filter(product => product.category === selectedFilter);
     }
+
+    if (searchQuery) {
+      updatedProducts = updatedProducts.filter(product => product.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    }
+
+    setFilteredProducts(updatedProducts);
     setCurrentPage(1);
-  }, [selectedFilter, products]);
+  }, [selectedFilter, searchQuery, products]);
 
   useEffect(() => {
     let sortedProducts = [...filteredProducts];
@@ -43,7 +50,7 @@ const ProductList = ({ products, addToCart }) => {
         sortedProducts.sort((a, b) => a.price - b.price);
         break;
       case 'rating':
-        sortedProducts.sort((a, b) => b.rating - a.rating); 
+        sortedProducts.sort((a, b) => b.rating - a.rating);
         break;
       default:
         break;
@@ -99,7 +106,7 @@ const ProductList = ({ products, addToCart }) => {
     'All Products',
     'Living Room',
     'Bedroom',
-     'Dining Room',
+    'Dining Room',
     'Table',
   ];
 
@@ -118,16 +125,26 @@ const ProductList = ({ products, addToCart }) => {
     setSortBy(sort);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div id='ourproducts' className='flex flex-col w-[95vw] gap-10 mx-auto tablet:px-6 phone:px-4 pt-5 bg-white phone:pt-0'>
       <div className='flex flex-col items-center justify-start gap-9'>
-      <div className="relative w-full rounded-[10px]">
-        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <img src={search} />
-        </span>
-        <input type="text" className="w-full pl-10 pr-4 py-2 border border-[#343A40]rounded-[10px] focus:outline-none focus:border-blue-500 borR" placeholder="Search"/>
-    </div>
-        <div className='flex flex-row justify-between w-full mx-auto tablet:flex-col tablet:gap-4  phone:gap-4 text-[#343A40]'>
+        <div className="relative w-full rounded-[10px]">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <img src={search} alt="Search Icon" />
+          </span>
+          <input
+            type="text"
+            className="w-full pl-10 pr-4 py-2 border border-[#343A40] rounded-[10px] focus:outline-none"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <div className='flex flex-row justify-between w-full mx-auto tablet:flex-col tablet:gap-4 phone:gap-4 text-[#343A40]'>
           {window.innerWidth < 768 ? (
             <>
               <select
@@ -163,13 +180,13 @@ const ProductList = ({ products, addToCart }) => {
                 ))}
               </div>
               <div className='flex flex-row items-center justify-between gap-6 text-[#747373] font-semibold tablet:flex-wrap phone:flex-wrap'>
-              <select value={sortBy} onChange={(e) => handleSortChange(e.target.value)}>
-        {sortOptions.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+                <select value={sortBy} onChange={(e) => handleSortChange(e.target.value)}>
+                  {sortOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
               </div>
             </>
           )}
@@ -188,14 +205,14 @@ const ProductList = ({ products, addToCart }) => {
                   <div className='flex flex-col gap-1'>
                     <p className="font-semibold text-2xl">£{product.price.toFixed(2)}</p>
                     <p className='text-xs font-normal flex flex-row gap-1 items-center'>
-                      <img src={star} alt="" />
-                      <span>{product.rating} </span>
+                      <img src={star} alt="Star" />
+                      <span>{product.rating}</span>
                       <span className='text-[#747373]'> ({product.review} Reviews) </span>
                     </p>
                   </div>
                 </div>
                 <div className='flex flex-row gap-2'>
-                  <button onClick={() => handleAddToCart(product)}><img src={addCart} alt="" /></button>
+                  <button onClick={() => handleAddToCart(product)}><img src={addCart} alt="Add to Cart" /></button>
                   <button onClick={() => handleAddToFavorites(product)}>
                     <img src={favorites.includes(product.id) ? heartFilled : heart} alt="Favorite" />
                   </button>
@@ -204,38 +221,23 @@ const ProductList = ({ products, addToCart }) => {
             </div>
           ))}
         </div>
-        {notificationItem && (
-          <AddToCartNotification item={notificationItem} onClose={() => setNotificationItem(null)} />
-        )}
-        <div className='flex justify-center items-center mt-4 w-full'>
-          <div className='flex gap-2'>
-            <button
-              className={`px-3 py-2 mx-2 rounded-[24px] ${currentPage === 1 ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-[#343A40] text-white'}`}
-              onClick={handlePrevPage}
-              disabled={currentPage === 1}
-            >
-              <img src={leftArrow} alt="Previous Page" />
-            </button>
-            {Array.from({ length: totalPages }, (_, index) => (
-              <button
-                key={index + 1}
-                onClick={() => handlePageChange(index + 1)}
-                className={`px-4 py-2 mx-1 rounded-[24px] ${currentPage === index + 1 ? 'bg-[#343A40] text-white' : 'bg-white text-[#343A40]'}`}
-              >
-                {index + 1}
-              </button>
-            ))}
-            <button
-              className={`rounded-[24px] px-3 py-2 mx-2 ${currentPage === totalPages ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-[#343A40] text-white'}`}
-              onClick={handleNextPage}
-              disabled={currentPage === totalPages}
-            >
-              <img src={rightArrow} alt="Next Page" />
-            </button>
+        {notificationItem && <AddToCartNotification item={notificationItem} />}
+        <div className="flex justify-between items-center w-full mt-4">
+          <button
+            className={`text-sm font-semibold ${currentPage === 1 ? 'invisible' : 'visible'}`}
+            onClick={handlePrevPage}
+          >
+            <img src={leftArrow} alt="Previous" />
+          </button>
+          <div className="flex items-center gap-2 text-sm font-semibold">
+            <span>{resultStart} - {resultEnd} of {filteredProducts.length} Results</span>
           </div>
-        </div>
-        <div className='mt-2 text-[#343A40]'>
-          Result {resultStart}-{resultEnd} of {filteredProducts.length}
+          <button
+            className={`text-sm font-semibold ${currentPage === totalPages ? 'invisible' : 'visible'}`}
+            onClick={handleNextPage}
+          >
+            <img src={rightArrow} alt="Next" />
+          </button>
         </div>
       </div>
     </div>
@@ -243,17 +245,7 @@ const ProductList = ({ products, addToCart }) => {
 };
 
 ProductList.propTypes = {
-  products: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      price: PropTypes.number.isRequired,
-      pic: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-      review: PropTypes.number.isRequired,
-      category: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+  products: PropTypes.array.isRequired,
   addToCart: PropTypes.func.isRequired,
 };
 
